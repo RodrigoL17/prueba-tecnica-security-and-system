50073531a2295d933354040eb43cf69471bf8d58
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-04 10:49:20 -0300

    Fix: Add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import styles from "./page.module.css";
+import Header from "@/app/ui/header/header";
+import styles from "@/app/page.module.css";
+import About from "@/app/ui/about/about";
+import Interest from "@/app/ui/interest/interest";
+import Contact from "@/app/ui/contact/contact";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <About />
+        <Interest />
+        <Contact />
+      </main>
+    </>
+
   );
 }
