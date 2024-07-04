d797de34dc94a95fc3dec3065aa4135ee2e3e138
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-04 10:49:44 -0300

    Feature: Contact section

---
+++ src/app/ui/contact/contact.tsx
@@ -0,0 +1,12 @@
+
+
+import styles from "@/app/ui/contact/contact.module.css";
+import Form from "@/app/ui/contact/form";
+export default function Contact() {
+  return (
+    <section id="contact" className={styles.section}>
+      <h2 className={styles.title}>Contactame</h2>
+      <Form />
+    </section>
+  );
+}
